ea21e15c95cfc6a8cfeb16f2a16890db4a61c87a
danylott <d.tuytuyshkin@gmail.com>
2020-11-30 20:53:02 +0200

    admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Passenger, Train, Trip, Carriage, Luggage
+
+
+admin.site.register(Passenger)
+admin.site.register(Train)
+admin.site.register(Trip)
+admin.site.register(Carriage)
+admin.site.register(Luggage)
