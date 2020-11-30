d966fee56fd36fe6088a13c44d50d5069a09383e
danylott <d.tuytuyshkin@gmail.com>
2020-11-30 20:55:54 +0200

    added viewsets

--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Passenger, Train, Trip, Carriage, Luggage
+from .serializers import PassengerSerializer, TrainSerializer, TripSerializer, CarriageSerializer, LuggageSerializer
+
+
+class PassengerViewSet(viewsets.ModelViewSet):
+    queryset = Passenger.objects.all()
+    serializer_class = PassengerSerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
+
+
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+
+class CarriageViewSet(viewsets.ModelViewSet):
+    queryset = Carriage.objects.all()
+    serializer_class = CarriageSerializer
+
+
+class LuggageViewSet(viewsets.ModelViewSet):
+    queryset = Luggage.objects.all()
+    serializer_class = LuggageSerializer
